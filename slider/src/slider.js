cee37dda58daabdc2435128b3e2b34f2ec71a98e
Hami Berkay Aktaş <45573382+hberkayaktas@users.noreply.github.com>
2022-03-21 16:40:35 +0300

    Add files via upload

--- slider/src/slider.js
+++ slider/src/slider.js
@@ -1,1 +1,32 @@
+var landing = document.querySelector("#landing > img").src;
+// console.log(landing);
+ var img_all = document.querySelectorAll("#mini_landing > ul > li > img");
+ //console.log(img_all);
+ var tum_liste = [];
+ for(var i =0; i<img_all.length; i++ ){
+       tum_liste[i] = img_all[i].src;
+     //  console.log(img_all[i].src);
+    //   console.log(tum_liste);
+ }
 
+
+
+ var vars =0;  
+ function degistir(){ 
+       if(vars < tum_liste.length){
+             document.querySelector("#landing > img").src = tum_liste[vars];
+             var prog =0;
+             var opa =0;
+             function progress_artir(){
+                   if(prog<101){
+                         document.querySelector("#timer > progress").value = prog;
+                         document.querySelector("#landing > img").style.opacity = opa;
+                   }
+                   prog++;
+                   if(opa<1){opa = opa + 0.01;}
+                   
+             }
+             setInterval(progress_artir,5);
+             vars ++ ;
+       }else{ vars = 0 ; }    
+ }
